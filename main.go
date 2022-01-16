2726117ce52aefd26a16a78071a5a6771724bdfb
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-01-16 17:23:20 +0100

    Simple Hello World Go Server

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		log.Println("Hello World")
+		d, _ := ioutil.ReadAll(r.Body)
+		log.Printf("Data %s\n", d)
+
+		fmt.Fprintf(rw, "Hello World %s\n", d)
 	})
+
+	http.HandleFunc("/video", func(http.ResponseWriter, *http.Request) {
+		log.Println("The video is supposed to be read and shown here")
+	})
+
 	http.ListenAndServe(":9090", nil)
 }
